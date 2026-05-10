84d82a07274f3c4f3775afb06ac0630c81802a81
Thiagarajan Baskarasubramanian <itthiagu@gmail.com>
2026-05-10 10:51:55 +0530

    Fixed a small bug for Second-Job

--- scripts/tb-myfirst-script.sh
+++ scripts/tb-myfirst-script.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "You are executing the script inside this repository"
 echo "This is a practice repository for GitHub Actions"
 echo "This script is being executed as part of a GitHub Actions workflow"
